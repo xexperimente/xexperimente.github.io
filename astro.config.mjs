f1753ef02e6554689e766d3706a888a9032084ab
Petr Kadlec <xexperimente@gmail.com>
2022-11-19 13:19:24 +0100

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import robotsTxt from 'astro-robots-txt';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://xexperimente.github.io',
-  base: '/xexperimente.github.io',
   integrations: [
     tailwind(),
     robotsTxt()
--- src/pages/index.md
+++ src/pages/index.md
@@ -3,7 +3,6 @@ layout: ../layouts/xexperimente.astro
 title: xexperimente's page
 description: home site of github.com/xexperimente
 ---
-
 # #introduction
 
 My name is Petr Kadlec.
